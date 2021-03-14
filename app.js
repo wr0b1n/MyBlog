b838c8839b1c75dad0d50325108ff3a5514d9b4f
wr0b1n <robin-lutter@gmx.de>
2021-03-14 15:05:18 +0100

    changed database url to mongo db atlas cloud

--- app.js
+++ app.js
@@ -3,6 +3,7 @@
 const express = require("express");
 const bodyParser = require("body-parser");
 const ejs = require("ejs");
+const mongoose = require("mongoose");
 const _ = require("lodash");
 
 const homeStartingContent = "Lacus vel facilisis volutpat est velit egestas dui id ornare. Semper auctor neque vitae tempus quam. Sit amet cursus sit amet dictum sit amet justo. Viverra tellus in hac habitasse. Imperdiet proin fermentum leo vel orci porta. Donec ultrices tincidunt arcu non sodales neque sodales ut. Mattis molestie a iaculis at erat pellentesque adipiscing. Magnis dis parturient montes nascetur ridiculus mus mauris vitae ultricies. Adipiscing elit ut aliquam purus sit amet luctus venenatis lectus. Ultrices vitae auctor eu augue ut lectus arcu bibendum at. Odio euismod lacinia at quis risus sed vulputate odio ut. Cursus mattis molestie a iaculis at erat pellentesque adipiscing.";
@@ -16,18 +17,32 @@ app.set('view engine', 'ejs');
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static("public"));
 
+// connect to database
+mongoose.connect("mongodb+srv://admin-robin:" + encodeURIComponent("269Ps#Mq") + "@cluster0.ffgrq.mongodb.net/blogDB", {useNewUrlParser: true});
 
+// schema for post
+const postSchema = {
+  title: String,
+  content: String
+};
 
-// global storage for posts
-let posts = [];
-
+// create model of schema
+const Post = mongoose.model("Post", postSchema);
 
 // routes - GET
 app.get("/", function (req, res) {
-  res.render("home", {
-    startingContent: homeStartingContent,
-    posts: posts
+
+  Post.find({}, function(err, posts){
+    res.render("home", {
+      startingContent: homeStartingContent,
+      posts: posts
+      });
   });
+
+});
+
+app.get("/compose", function (req, res) {
+  res.render("compose");
 });
 
 app.get("/about", function (req, res) {
@@ -38,36 +53,32 @@ app.get("/contact", function (req, res) {
   res.render("contact", { contactContent: contactContent });
 });
 
-app.get("/compose", function (req, res) {
-  res.render("compose");
-});
 
 // dynamic parameter
-app.get("/posts/:postName", function (req, res) {
-  const requestedTitle = _.lowerCase(req.params.postName);
+app.get("/posts/:postId", function (req, res) {
+  const requestedPostId = req.params.postId;
 
-  posts.forEach(function (post) {
-    const storedTitle = _.lowerCase(post.title);
-
-    if (storedTitle == requestedTitle) {
-      res.render("post", {
-        title: post.title,
-        content: post.content
-      });
-    }
+  Post.findOne({_id: requestedPostId}, function(err, post){
+    res.render("post", {
+      title: post.title,
+      content: post.content
+    });
   });
 });
 
 
-// routes - POST
+// routes - POST --> save post in database
 app.post("/compose", function (req, res) {
-  const post = {
+  const post = new Post ({
     title: req.body.postTitle,
     content: req.body.postBody
-  };
+  });
 
-  posts.push(post);
-  res.redirect("/");
+  post.save(function(err){
+    if (!err){
+      res.redirect("/");
+    }
+  });
 });
 
 
